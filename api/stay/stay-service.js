e880ad278d3cee763b781247452ec65119c50241
Liran Parti <liranpar@gmail.com>
2022-03-30 21:46:21 +0300

    built filter in backend

--- api/stay/stay-service.js
+++ api/stay/stay-service.js
@@ -1,101 +1,109 @@
-const dbService = require('../../services/db-service')
-const logger = require('../../services/logger-service')
-const ObjectId = require('mongodb').ObjectId
-
-
+const dbService = require("../../services/db-service");
+const logger = require("../../services/logger-service");
+const ObjectId = require("mongodb").ObjectId;
 
 async function query(filterBy) {
-    try {
-        // const criteria = _buildCriteria(filterBy);
-        // const sort = _buildSort(filterBy)
-        const collection = await dbService.getCollection('stay')
-        var stays = await collection.find().toArray()
-        return stays
-    } catch (err) {
-        logger.error('cannot find stays', err)
-        throw err
-    }
+  try {
+    const criteria = _buildCriteria(filterBy);
+    // const sort = _buildSort(filterBy)
+    const collection = await dbService.getCollection("stay");
+    var stays = await collection.find(criteria).toArray();
+    return stays;
+  } catch (err) {
+    logger.error("cannot find stays", err);
+    throw err;
+  }
 }
 
 async function getById(stayId) {
-    try {
-        const collection = await dbService.getCollection('stay')
-        const stay = collection.findOne({ _id: ObjectId(stayId) })
-        return stay
-    } catch (err) {
-        logger.error(`while finding stay ${stayId}`, err)
-        throw err
-    }
+  try {
+    const collection = await dbService.getCollection("stay");
+    const stay = collection.findOne({ _id: ObjectId(stayId) });
+    return stay;
+  } catch (err) {
+    logger.error(`while finding stay ${stayId}`, err);
+    throw err;
+  }
 }
 
 async function remove(stayId) {
-    try {
-        const collection = await dbService.getCollection('stay')
-        await collection.deleteOne({ '_id': ObjectId(stayId) })
-        return stayId
-    } catch (err) {
-        logger.error(`cannot remove stay ${stayId}`, err)
-        throw err
-    }
+  try {
+    const collection = await dbService.getCollection("stay");
+    await collection.deleteOne({ _id: ObjectId(stayId) });
+    return stayId;
+  } catch (err) {
+    logger.error(`cannot remove stay ${stayId}`, err);
+    throw err;
+  }
 }
 
 async function add(stay) {
-    try {
-        stay.createdAt = Date.now();
-        const collection = await dbService.getCollection('stay')
-        const addedStay = await collection.insertOne(stay)
-        return addedStay
-    } catch (err) {
-        logger.error('cannot insert stay', err)
-        throw err
-    }
+  try {
+    stay.createdAt = Date.now();
+    const collection = await dbService.getCollection("stay");
+    const addedStay = await collection.insertOne(stay);
+    return addedStay;
+  } catch (err) {
+    logger.error("cannot insert stay", err);
+    throw err;
+  }
 }
 async function update(stay) {
-    try {
-        var id = ObjectId(stay._id)
-        delete stay._id
-        const collection = await dbService.getCollection('stay')
-        await collection.updateOne({ "_id": id }, { $set: { ...stay } })
-        stay._id = id;
-        return stay
-    } catch (err) {
-        logger.error(`cannot update stay ${id}`, err)
-        throw err
-    }
+  try {
+    var id = ObjectId(stay._id);
+    delete stay._id;
+    const collection = await dbService.getCollection("stay");
+    await collection.updateOne({ _id: id }, { $set: { ...stay } });
+    stay._id = id;
+    return stay;
+  } catch (err) {
+    logger.error(`cannot update stay ${id}`, err);
+    throw err;
+  }
 }
 
+function _buildCriteria(filterBy = {}) {
+  const criteria = {};
 
-// function _buildCriteria(filterBy) {
-//     const criteria = {};
-//     if (filterBy.name) {
-//         const nameCriteria = { $regex: filterBy.name, $options: 'i' }
-//         criteria.name = nameCriteria
-//     }
-    
-//     if (filterBy.inStock === 'true' || filterBy.inStock === 'false') {
-//         criteria.inStock = (filterBy.inStock === "true")? true: false
+  if (filterBy.guests) {
+    criteria.capacity = { $gte: +filterBy.guests };
+  }
+  if (filterBy.price && filterBy.price.length) {
+    criteria["$and"] = [
+      {
+        price: { $gte: +filterBy.price[0] },
+      },
+      {
+        price: { $lte: +filterBy.price[1] },
+      },
+    ];
+  }
 
-//     }
+  if (filterBy.type && filterBy.type.length) {
+    criteria.propertyType = { $in: filterBy.type };
+  }
 
-//     if (filterBy.labels) {
-//         criteria.labels = { $in: filterBy.labels }
-//     }
+  if (filterBy.amenities && filterBy.amenities.length > 0) {
+    criteria.amenities = { $all: filterBy.amenities };
+  }
 
-//     return criteria;
-// }
+  if (filterBy.city) {
+    criteria["address.city"] = { $regex: filterBy.city, $options: "i" };
+  }
 
-// function _buildSort({ sortBy }) {
-//     if(!sortBy) return {}
+  return criteria;
+}
 
-//     return {[sortBy] :1}
-// }
+function _buildSort({ sortBy }) {
+  if (!sortBy) return {};
 
-
+  return { [sortBy]: 1 };
+}
 
 module.exports = {
-    remove,
-    query,
-    getById,
-    add,
-    update,
-}
+  remove,
+  query,
+  getById,
+  add,
+  update,
+};
