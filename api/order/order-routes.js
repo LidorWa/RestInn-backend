d8d8014a6e5797eb65c66574ab2b78ed2e6e2baa
Lidor Waldman <lidorvl@gmail.com>
2022-03-30 09:57:46 +0300

    checks in postman dropped the auth checks to ensure connection btwn db and service

--- api/order/order-routes.js
+++ api/order/order-routes.js
@@ -1,24 +1,21 @@
 const express = require('express')
-const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth-middleware')
+const { requireAuth, requireHost } = require('../../middlewares/requireAuth-middleware')
 const { log } = require('../../middlewares/logger-middleware')
 const { getOrders, getOrderById, addOrder, updateOrder, removeOrder, addReview } = require('./order-controller.js')
 const router = express.Router()
 
-// middleware that is specific to this router
-// router.use(requireAuth)
 
-// router.get('/', log, getOrders)
-// router.get('/:id', getOrderById)
-// router.post('/', addOrder)
-// router.put('/:id',  updateOrder)
-// router.delete('/:id', removeOrder)
+// router.get('/',  log, getOrders)
+// router.get('/:id',log, requireAuth, requireHost, getOrderById)
+// router.post('/',log, requireAuth, requireHost, addOrder)
+// router.put('/:id',log, requireAuth, requireHost, updateOrder)
+// router.delete('/:id',log, requireAuth, requireHost, removeOrder)
 
+router.get('/',  log, getOrders)
+router.get('/:id',log, getOrderById)
+router.post('/',log, addOrder)
+router.put('/:id',log, updateOrder)
+router.delete('/:id',log, removeOrder)
 
-
-router.get('/', log, getOrders)
-router.get('/:id', getOrderById)
-router.post('/', requireAuth, requireHost, addOrder)
-router.put('/:id', requireAuth, requireHost, updateOrder)
-router.delete('/:id', requireAuth, requireHost, removeOrder)
 
 module.exports = router
--- api/user/user-routes.js
+++ api/user/user-routes.js
@@ -1,14 +1,24 @@
 const express = require('express')
-const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth-middleware')
-const {getUser, getUsers, deleteUser, updateUser} = require('./user-controller')
+const { requireAuth, requireHost } = require('../../middlewares/requireAuth-middleware')
+const { getUser, getUsers, deleteUser, addUser, updateUser } = require('./user-controller')
 const router = express.Router()
 
-// middleware that is specific to this router
-// router.use(requireAuth)
 
-router.get('/', getUsers)
+router.get('/', getUsers)  
 router.get('/:id', getUser)
-router.put('/:id',  requireAuth, updateUser)
-router.delete('/:id',  requireAuth, requireAdmin, deleteUser)
+router.put('/:id', updateUser)
+router.post('/', addUser) 
+router.delete('/:id', deleteUser)
+
+
+// router.get('/', getUsers)  
+// router.get('/:id', getUser)
+// router.put('/:id',  requireAuth, updateUser)
+// router.post('/',  requireAuth, updateUser) 
+// router.delete('/:id',  requireAuth, requireHost, deleteUser)
 
 module.exports = router
+
+
+// TODO: Check with Tom - are the users in the data we received hosts or regular users?
+// TODO: Check with Tom - should the users password be shown in the database, or hashed? because Sharon's DB passwords are regular.
