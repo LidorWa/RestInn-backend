4b4ccd81797f4a685b9de15ffbfa7c797e73fd40
Lidor Waldman <lidorvl@gmail.com>
2022-04-01 19:24:42 +0300

    trying to connect sockets feature

--- api/user/user-routes.js
+++ api/user/user-routes.js
@@ -18,7 +18,3 @@ router.delete('/:id', deleteUser)
 // router.delete('/:id',  requireAuth, requireHost, deleteUser)
 
 module.exports = router
-
-
-// TODO: Check with Tom - are the users in the data we received hosts or regular users?
-// TODO: Check with Tom - should the users password be shown in the database, or hashed? because Sharon's DB passwords are regular.
